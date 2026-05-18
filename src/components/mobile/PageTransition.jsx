a1bb543c73afbb38bbbb0f9e5aa0ba5c4358870a
yuval lavy <yuvallavy@gmail.com>
2026-05-18 19:53:22 +0000

    File changes

--- src/components/mobile/PageTransition.jsx
+++ src/components/mobile/PageTransition.jsx
@@ -11,10 +11,11 @@ export default function PageTransition({ children }) {
 
   return (
     <motion.div
-      initial={{ opacity: 0, x: 20 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: -20 }}
-      transition={{ duration: 0.3 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.2 }}
+      style={{ overflowX: 'hidden' }}
     >
       {children}
     </motion.div>
